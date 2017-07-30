05f34011820fe34890103883a71a1a24f0b2769f
Kai Harries <kai.harries@gmail.com>
2017-07-30 13:16:37 +0200

    Remove tails 2 from grub menu

--- configuration.nix
+++ configuration.nix
@@ -63,15 +63,6 @@
           linux (loop)/live/vmlinuz boot=live config fromiso=/dev/sda1/$isofile
           initrd (loop)/live/initrd.img
       }
-      menuentry "Bootable ISO Image: Tails 2" {
-          insmod part_gpt
-          insmod fat
-          set root='hd0,1'
-          set isofile='/images/tails2.iso'
-          loopback loop $isofile
-          linux (loop)/live/vmlinuz2 boot=live config findiso=/images/tails2.iso apparmor=1 security=apparmor nopersistence noprompt timezone=Etc/UTC block.events_dfl_poll_msecs=1000 noautologin module=Tails kaslr slab_nomerge slub_debug=FZ mce=0 vsyscall=none  
-          initrd (loop)/live/initrd2.img
-      }
       menuentry "Bootable ISO Image: Tails 3" {
           insmod part_gpt
           insmod fat
