54b242b2ab37589f609b4c645815b99027d127a8
Kai Harries <kai.harries@gmail.com>
2020-04-19 18:19:33 +0200

    Merge services.nix and systemPackages.nix back into configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -12,8 +12,6 @@ in
 with pkgs; {
   imports = [
     ./hardware-configuration.nix
-    ./services.nix
-    ./systemPackages.nix
   ];
 
   nixpkgs = {
@@ -180,4 +178,136 @@ with pkgs; {
     kai ALL = NOPASSWD : /run/current-system/sw/bin/poweroff
     kai ALL = NOPASSWD : /run/current-system/sw/bin/reboot
     '';
+
+    services = {
+
+    dbus = {
+      enable = true;
+    };
+
+    fstrim = {
+      enable = true;
+    };
+
+    journald.extraConfig = "SystemMaxUse=128M";
+
+    nullmailer = {
+      enable = true;
+      config.allmailfrom = "postmaster.rob@gmail.com";
+      remotesFile = "/etc/nullmailer.remotes";
+    };
+
+    openssh = {
+      enable = true;
+      challengeResponseAuthentication = false;
+      passwordAuthentication = false;
+      permitRootLogin = "no";
+    };
+
+    pcscd.enable = true;
+    physlock.enable = true;
+
+    printing = {
+      drivers = [ gutenprint ];
+      enable = true;
+    };
+
+    dnsmasq = {
+      enable = true;
+      servers = [ "127.0.0.1#53000" ];
+    };
+
+    resolved.enable = false;
+    stubby = {
+      enable = true;
+      listenAddresses = [ "127.0.0.1@53000"
+                          "0::1@53000" ];
+    };
+
+    spice-vdagentd.enable = true;
+    vnstat.enable = true;
+
+    xserver = {
+      enable = true;
+      layout = "us";
+      displayManager.slim = {
+        enable = true;
+        defaultUser = "kai";
+        autoLogin = false;
+        extraConfig = ''
+          sessionstart_cmd    ${xorg.sessreg}/bin/sessreg -a -l tty7 %user
+          sessionstop_cmd     ${xorg.sessreg}/bin/sessreg -d -l tty7 %user
+        '';
+      };
+      desktopManager.default = "none";
+      windowManager = {
+        default = "xmonad";
+        xmonad.enable = true;
+      };
+    };
+  };
+
+  environment.systemPackages = with pkgs; [
+    adapta-gtk-theme
+    alsaUtils
+    binutils
+    cdrkit
+    chromium
+    cifs-utils
+    debootstrap
+    dfu-programmer
+    dnsutils
+    dstat
+    efibootmgr
+    evince
+    feh
+    file
+    firefox
+    fontconfig
+    gimp
+    git
+    gitAndTools.gitFull
+    gnome3.adwaita-icon-theme
+    gnome3.dconf
+    gnumake
+    gnupg
+    gparted
+    graphviz
+    hicolor_icon_theme
+    kvm
+    linuxPackages.perf
+    lshw
+    nethogs
+    nfs-utils
+    nitrokey-app
+    nix-prefetch-scripts
+    nix-zsh-completions
+    pandoc
+    parted
+    pass
+    pavucontrol
+    pciutils
+    pcmanfm
+    pdftk
+    psmisc
+    pwgen
+    python3
+    quilt
+    rsync
+    shotwell
+    stdenv
+    syslinux
+    transmission_gtk
+    tree
+    unzip
+    usbutils
+    usermount
+    vim
+    virt-viewer
+    virtmanager
+    vnstat
+    w3m
+    wireshark
+    wol
+  ];
 }
